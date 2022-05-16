5bfbdc788fecb3df7342804862ba7c5aaeaf6eb9
rodriguestg <grs.ibr@gmail.com>
2022-05-16 10:10:07 -0300

    Subindo requisito 2

--- src/average.js
+++ src/average.js
@@ -27,6 +27,6 @@ const average = (numeros) => {
   return mediaArredondada;
 };
 
-console.log(average([1, 2, 3, '4', 5]));
+// console.log(average([1, 2, 3, '4', 5]));
 
 module.exports = average;
--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,10 +16,14 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    expect(true).toEqual(numbers([1, 2, 3, 4, 5]));
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    expect(false).toEqual(numbers([1, 2, '3', 4, 5]));
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    expect(false).toEqual(numbers([1, 'a', 3]));
     // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(false).toEqual(numbers([' ']));
   });
 });
